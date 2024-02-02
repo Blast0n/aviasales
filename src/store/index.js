d041b01f900b0a351185c7e89e2ae1621703671f
Ilya Pronyakin <blastinboosting@gmail.com>
2024-02-02 08:26:08 +0300

    ddadsa

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { configureStore } from '@reduxjs/toolkit';
 
-import appSlice from './AppSlice';
+import appSlice from './appSlice';
 
 export default configureStore({
   reducer: {
